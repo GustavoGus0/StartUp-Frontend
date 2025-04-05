852d4da8a6c2c32d8ba35bac42974c7a94f6795f
GustavoGus0 <manunknown964@gmail.com>
2025-04-05 15:52:12 +0500

    Initialized project on react 19 and typescript using vite and pnpm.

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router'
+
+import App from './App.tsx'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </StrictMode>
+)
